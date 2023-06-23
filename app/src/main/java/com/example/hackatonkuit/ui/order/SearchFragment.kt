805e899ed2ca61d49356d03c1ea70f44efbb50df
mjk02123 <k64837611@gmail.com>
2023-06-24 01:12:51 +0900

    order 찐완 다듬음.
    1. allmenu 크기 조정
    2. bottombar 색깔
    3. tab2에서 안 보이게

---
+++ app/src/main/java/com/example/hackatonkuit/ui/order/SearchFragment.kt
@@ -0,0 +1,23 @@
+package com.example.hackatonkuit.ui.order
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.hackatonkuit.R
+import com.example.hackatonkuit.databinding.FragmentMyMenuBinding
+import com.example.hackatonkuit.databinding.FragmentSearchBinding
+
+class SearchFragment : Fragment() {
+    lateinit var binding: FragmentSearchBinding
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentSearchBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+}
