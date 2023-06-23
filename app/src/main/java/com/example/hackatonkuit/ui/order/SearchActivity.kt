37e10e71c3958077f9a1a04773833f67b6cae0d0
nonaninona <starcraft0529@gmail.com>
2023-06-24 08:38:47 +0900

    SearchActivity 연결

--- app/src/main/java/com/example/hackatonkuit/ui/order/SearchActivity.kt
+++ app/src/main/java/com/example/hackatonkuit/ui/order/SearchActivity.kt
@@ -1,9 +1,25 @@
 package com.example.hackatonkuit.ui.order
 
-import android.os.Bundle
-import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.WindowManager
+import com.example.hackatonkuit.R
+import com.example.hackatonkuit.databinding.ActivitySearchBinding
 
 class SearchActivity : AppCompatActivity() {
+    lateinit var binding: ActivitySearchBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySearchBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initLayout()
+    }
 
+    fun initLayout() {
+        if (binding.searchEt.requestFocus())
+            getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_VISIBLE);
+        binding.deleteTv.setOnClickListener {
+            finish()
+        }
+    }
 }
