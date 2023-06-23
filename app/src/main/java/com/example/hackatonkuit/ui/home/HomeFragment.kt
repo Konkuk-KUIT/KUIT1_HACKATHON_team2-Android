730d6ad1bc3ea3260c550ae3be75a9686e57a0a8
yskim6772 <yskim6772@gmail.com>
2023-06-23 17:50:11 +0900

    recyclerview adapter 하는중 ...

--- app/src/main/java/com/example/hackatonkuit/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/hackatonkuit/ui/home/HomeFragment.kt
@@ -2,21 +2,39 @@ package com.example.hackatonkuit.ui.home
 
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.view.menu.MenuAdapter
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.hackatonkuit.R
 import com.example.hackatonkuit.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
 
-    lateinit var binding : FragmentHomeBinding
+    private lateinit var binding : FragmentHomeBinding
+    private lateinit var menuAdapter: MenuAdapter
 
+    val items = mutableListOf<android.view.MenuItem>()
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentHomeBinding.inflate(inflater, container, false)
+
+        menuAdapter = MenuAdapter()
+        //binding.homeRecyclerView.layoutManager = LinearLayoutManager(requireContext())
+        //binding.homeRecyclerView.adapter = menuAdapter
+
+        val menuItems = listOf(
+            MenuItem(R.drawable.americano, "아메리카노")
+        )
+        //menuAdapter.submitList(menuItems)
 
         binding.homeNewaccountButton.setOnClickListener {
             //회원가입 버튼 클릭 시
@@ -27,6 +45,35 @@ class HomeFragment : Fragment() {
         }
         return binding.root
 
+    }
+
+
+    data class MenuItem(
+        val menuimage: Int,
+        val menuname: String
+        )
+
+    class MenuAdapter : RecyclerView.Adapter<MenuAdapter.MenuViewHolder>(){
+        private var menuItems: List<MenuItem> = emptyList()
+
+
+        //외부에서 데이터 리스트를 설정
+        fun setMenuList(menuItems: List<MenuItem>){
+            this.menuItems = menuItems
+            notifyDataSetChanged()
+        }
+
+
+        //RecyclerView의 아이템 뷰를 위한 뷰 홀더 클래스
+        inner class MenuViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+            private val imageView: ImageView = itemView.findViewById<ImageView>(R.id.home_new_image)
+            private val textView: TextView = itemView.findViewById(R.id.home_new_name)
+
+            fun bind(menuItem: MenuItem) {
+                imageView.setImageResource(menuItem.menuimage)
+                textView.text = menuItem.menuname
+            }
+        }
     }
 
 
---
+++ app/src/main/java/com/example/hackatonkuit/ui/home/MenuAdapter.kt
@@ -0,0 +1,34 @@
+package com.example.hackatonkuit.ui.home
+
+import android.view.LayoutInflater
+import android.view.MenuItem
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.hackatonkuit.databinding.FragmentHomeBinding
+
+class MenuAdapter: RecyclerView.Adapter<MenuAdapter.MyViewHolder>() {
+
+    var datalist = mutableListOf<HomeFragment>()
+
+    inner class MyViewHolder(private val binding: FragmentHomeBinding):RecyclerView.ViewHolder(binding.root){
+        fun bind(menuItem: MenuItem){
+            binding.homeNewName.text = MenuItem.menuname
+            binding.homeNewImage.id = HomeFragment.MenuItem.menuimage
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val binding = ItemMenuBinding.inflate(inflater, parent, false)
+        return MenuViewHolder(binding)
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        val menuItem = menuItems[position]
+        holder.bind(menuItem)
+    }
+}
